7ee964befc8d266f721e32d887511f6b1cdd8459
Hammad Hassan <Hafizhammad611@gmail.com>
2025-06-14 20:17:43 +0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "https://10ea-13-54-223-152.ngrok-free.app/api/auth/google/callback";
+export const BACKEND_URL = "https://de14-13-54-223-152.ngrok-free.app";
 
 export const API_ROUTES = {
     AUTH: {
